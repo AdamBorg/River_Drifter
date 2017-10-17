e9df21210e81580db1e0a9bf16dcd0eeca50261d
AdamBorg <adam.borg@my.jcu.edu.au>
2017-10-17 21:50:43 +1000

    wrote code to test motor control

---
+++ Test_Components/Sources/main.c
@@ -0,0 +1,121 @@
+/* ###################################################################
+**     Filename    : main.c
+**     Project     : Test_Components
+**     Processor   : MK20DX128VLH5
+**     Version     : Driver 01.01
+**     Compiler    : GNU C Compiler
+**     Date/Time   : 2017-10-17, 20:27, # CodeGen: 0
+**     Abstract    :
+**         Main module.
+**         This module contains user's application code.
+**     Settings    :
+**     Contents    :
+**         No public methods
+**
+** ###################################################################*/
+/*!
+** @file main.c
+** @version 01.01
+** @brief
+**         Main module.
+**         This module contains user's application code.
+*/         
+/*!
+**  @addtogroup main_module main module documentation
+**  @{
+*/         
+/* MODULE main */
+
+
+/* Including needed modules to compile this module/procedure */
+#include "Cpu.h"
+#include "Events.h"
+/* Including shared modules, which are used for whole project */
+#include "PE_Types.h"
+#include "PE_Error.h"
+#include "PE_Const.h"
+#include "IO_Map.h"
+/* User includes (#include below this line is not maintained by Processor Expert) */
+
+void turnLeft(){
+	  H1Motor_PutVal(0);
+	  H2Motor_SetRatio8(255);
+}
+
+void turnRight(){
+	  H1Motor_PutVal(1);
+	  H2Motor_SetRatio8(255);
+}
+
+void neutral(){
+	  H1Motor_PutVal(1);
+	  H2Motor_SetRatio8(0);
+}
+
+void keyPressed(){
+	bool keyPressed = FALSE;
+
+	while(keyPressed = FALSE){
+		keyPressed = Term1_KeyPressed();
+		}
+}
+
+/*lint -save  -e970 Disable MISRA rule (6.3) checking. */
+int main(void)
+/*lint -restore Enable MISRA rule (6.3) checking. */
+{
+  /* Write your local variable definition here */
+
+  /*** Processor Expert internal initialization. DON'T REMOVE THIS CODE!!! ***/
+  PE_low_level_init();
+  /*** End of Processor Expert internal initialization.                    ***/
+
+  char CharacterEntered;
+
+  for(;;){
+	  neutral();
+	  keyPressed();
+
+	  Term1_ReadChar(*CharacterEntered);
+	  Term1_SendChar(*CharacterEntered);
+
+	  switch (*CharacterEntered){
+	  case 'l':
+		  turnLeft();
+		  keyPressed();
+		  break;
+	  case 'r':
+		  turnRight();
+		  keyPressed();
+		  break;
+	  case 'n':
+		  neutral();
+		  keyPressed();
+		  break;
+	  }
+
+  }
+
+  /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
+  /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
+  #ifdef PEX_RTOS_START
+    PEX_RTOS_START();                  /* Startup of the selected RTOS. Macro is defined by the RTOS component. */
+  #endif
+  /*** End of RTOS startup code.  ***/
+  /*** Processor Expert end of main routine. DON'T MODIFY THIS CODE!!! ***/
+  for(;;){}
+  /*** Processor Expert end of main routine. DON'T WRITE CODE BELOW!!! ***/
+} /*** End of main routine. DO NOT MODIFY THIS TEXT!!! ***/
+
+/* END main */
+/*!
+** @}
+*/
+/*
+** ###################################################################
+**
+**     This file was created by Processor Expert 10.5 [05.21]
+**     for the Freescale Kinetis series of microcontrollers.
+**
+** ###################################################################
+*/
